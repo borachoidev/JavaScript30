a463aabb26df5099b299560df67accc96ca07837
september21st <september21st@users.noreply.github.com>
2021-03-23 22:58:46 +0900

    [기능] JS Clock 기본

---
+++ exercises/02 - JS and CSS Clock/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+
+  //시계바늘이 기존에 180도였기때문에 초침 각도에 90을 더해준다.
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
